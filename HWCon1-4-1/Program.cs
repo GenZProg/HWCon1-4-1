2e95eb56053f28dffa90c2e56b890b3e5252d7a6
GenZProg <GenZ.prog@outlook.com>
2021-12-12 11:09:07 +0800

    initial

---
+++ HWCon1-4-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWCon1_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!!");
+            ReadKey();
+
+        }
+    }
+}
